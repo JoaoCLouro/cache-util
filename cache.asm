8bc9ff400037a6a947db56c46b89fcd373c5d84a
babma04 <joaocarrilholouro@gmail.com>
2026-05-28 09:11:22 +0100

    _start validation to the access and get a cache

--- cache.asm
+++ cache.asm
@@ -31,34 +31,60 @@
 ; Cache details:
 ;
   SYSTEM_ADDRESS_SIZE EQU 64 
-  CACHE_BLOCK_SIZE EQU 64                           ; Bytes
+  CACHE_BLOCK_SIZE    EQU 64                           ; Bytes
   
 ; Changeable Values:
 
   CACHE_WAYS EQU 4
   CACHE_SIZE EQU 16777216
 
-  CACHE_LINES EQU (CACHE_SIZE / CACHE_BLOCK_SIZE)   ; 262144 lines
-  CACHE_CELL_SIZE EQU (CACHE_BLOCK_SIZE / CACHE_WAYS)
+  CACHE_LINES       EQU (CACHE_SIZE / CACHE_BLOCK_SIZE)   ; 262144 lines
+  CACHE_CELL_SIZE   EQU (CACHE_BLOCK_SIZE / CACHE_WAYS)
   
-  CACHE_INDEX_BITS EQU 16                           ; log 2 (CACHE_LINES / CACHE_WAYS)
+  CACHE_INDEX_BITS  EQU 16                           ; log 2 (CACHE_LINES / CACHE_WAYS)
   CACHE_OFFSET_BITS EQU 6                           ; log 2 (CACHE_BLOCK_SIZE)
-  CACHE_TAG_BITS EQU (SYSTEM_ADDRESS_SIZE - (CACHE_INDEX_BITS + CACHE_OFFSET_BITS)) ;42
+  CACHE_TAG_BITS    EQU (SYSTEM_ADDRESS_SIZE - (CACHE_INDEX_BITS + CACHE_OFFSET_BITS)) ;42
 ; --------------------------------------------
   
-section .bss
-align CACHE_BLOCK_SIZE         
-cache_validity:   resb (CACHE_LINES / 4)        ; needs to be changed to the correct racio for any other nº of ways
-cache_tags:     resb (CACHE_TAG_BITS * CACHE_WAYS * CACHE_LINES)
-cache_buffer:    resb CACHE_SIZE
+section .data
+validation_address: dq 0    ; Passkey to the cache
 
-section .text 
-global _start:
+section .bss
+align CACHE_BLOCK_SIZE
+cache_validity:   resb CACHE_LINES      ; 2 bits for each cache cell (1 byte total by block)
+cache_tags:       resb (CACHE_TAG_BITS * CACHE_WAYS * CACHE_LINES)
+cache_buffer:     resb CACHE_SIZE
+
+section .text
+global _start
+global _read_cache
+global _write_cache
 
 ; To Implement
 ;   WRITE CACHE
 
 
+
+; -----------------------------------------------------
+; _start:
+;       Returns a validation address to the cache
+; Outputs:
+;       RAX: address to use to access the cache
+; Destoys: 
+;       RAX, RBX
+; -----------------------------------------------------
+    _start:
+        ; gets a random value in the to randomize the address
+        xor RAX
+        xor RBX
+        _get_rand_val
+        mov rax, rbx
+        add rax, validation_address
+        mov [validation_address], rax
+        xor RBX
+        ret
+        
+    
 
 ; -----------------------------------------------------
 ; _read_cache:
@@ -133,7 +159,17 @@ global _start:
             xor RBX
             mov rax, 1
             ret
-            
+ 
+
+
+
+
+
+
+; --------------------------
+;   Multi purpose helpers
+; --------------------------
+
 
 ; --------------------------------------------
 ; _get_index_bits:
@@ -185,4 +221,28 @@ global _start:
         shr rax, CACHE_TAG_BITS + CACHE_INDEX_BITS
         ret
 
-        
+; --------------------------------------------
+; _get_rand_val:
+;       Returns a random 8 bytes value.
+;       Uses a syscall to get the random 8 bytes value.
+;
+; Outputs:
+;       RAX: random 8 bytes value
+; --------------------------------------------
+    _get_rand_val:
+        ; saves the stack state and reserves 8 bytes
+        push rbp
+        mov rbp, rsp
+        sub rsp, 8
+        ; random value syscall
+        mov rax, 318        
+        mov rdi, rsp        
+        mov rsi, 8          
+        mov rdx, 0          
+        syscall                
+        ; sends that value to the return register
+        mov rax, [rsp]
+        ; resets the stack
+        mov rsp, rbp
+        pop rbp
+        ret
